eecd816f98436ff26bf919caa74700c5ec30ea97
D-Development-SA <daelva16@gmail.com>
2023-03-25 18:00:19 -0400

    Inicio de mantenimiento y control de pegajoso

---
+++ src/main/java/com/deika/pegajoso/PegajosoApplication.java
@@ -0,0 +1,13 @@
+package com.deika.pegajoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegajosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegajosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deika/pegajoso/PegajosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deika.pegajoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegajosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
